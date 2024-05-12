c8e9f69c002a03abfcd2e47a9e372fc4cda5cf13
azimjonusmonov602 <azimjonusmonov602@gmail.com>
2024-05-12 08:42:05 +0500

    Person classi yaratildi

---
+++ src/main/java/oop_pracitce/Person.java
@@ -0,0 +1,31 @@
+package oop_pracitce;
+
+public class Person {
+    String fullName;
+    int age;
+    String gender;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
